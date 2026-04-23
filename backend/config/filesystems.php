df424b7eccc40f4c903204d8cd6fb5e812bed8e6
Developer <dev@laverdad.edu.ph>
2026-04-23 17:24:13 +0800

    done fixing config and build files

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -63,9 +63,9 @@ return [
         'cloudinary' => [
             'driver' => 'cloudinary',
             'url' => env('CLOUDINARY_URL'),
-            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-            'api_key' => env('CLOUDINARY_API_KEY'),
-            'api_secret' => env('CLOUDINARY_API_SECRET'),
+            'cloud' => env('CLOUDINARY_CLOUD_NAME'),
+            'key' => env('CLOUDINARY_API_KEY'),
+            'secret' => env('CLOUDINARY_API_SECRET'),
             'secure' => true,
         ],
 
